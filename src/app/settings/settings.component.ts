2a2d07e349bb431219d4959ab6f457c28ce85ac8
Denis Tomczyk <denis.schick@gmail.com>
2025-02-03 18:04:08 +0100

    Tabs added

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTabsModule } from "@angular/material/tabs";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [],
+  imports: [
+    MatTabsModule
+  ],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
-export class SettingsComponent {
+export class SettingsComponent implements OnInit{
+  catImageId: string | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.loadCatId();
+  }
+
+  loadCatId() {
+    this.http.get<any[]>('https://api.thecatapi.com/v1/images/search').subscribe({
+      next: (data) => {
+        if (data.length > 0) {
+          this.catImageId = data[0].id; // Statt URL die ID speichern
+        }
+      },
+      error: (err) => console.error('Fehler beim Laden der Cat-ID:', err)
+    });
+  }
 
 }
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
 import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(BrowserAnimationsModule)
+    importProvidersFrom(BrowserAnimationsModule),
+    provideHttpClient()
   ]
 }).catch(err => console.error(err));
